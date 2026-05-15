cd05538b93d5ccf25be5a93efbc025003b3903e5
Prem948 <premkumarsuresh@gmail.com>
2026-05-15 16:31:48 +0200

    fix: Update Calendly URL to the correct scheduling link

--- static/js/site-config.js
+++ static/js/site-config.js
@@ -1,5 +1,5 @@
 window.EAGLE_SITE_CONFIG = window.EAGLE_SITE_CONFIG || {
     contactApiUrl: "https://eagleweb-app.onrender.com/api/contact",
     whatsAppUrl: "https://wa.me/918778584642?text=Hello%20Eagle%20Group%2C%20I%20want%20a%20free%20consultation%20for%20a%20CRM%20or%20dashboard%20project.",
-    calendlyUrl: "https://calendly.com/eaglegroupit/free-consultation"
+    calendlyUrl: "https://calendly.com/premkumarsuresh/30min"
 };
